f2c79b7f5263a7fcedcfbdfa571a2e6db47b5d55
Ahmed Sultan <as11bs22@gmail.com>
2024-01-27 15:08:38 +0200

    added some dependencies

--- src/components/CheckoutCard.js
+++ src/components/CheckoutCard.js
@@ -1,4 +1,4 @@
-import React, { useState } from "react";
+import React from "react";
 import Steps from "./Steps";
 import Form from "./Form";
 import classes from "./CheckoutCard.module.css";
--- src/components/Steps.js
+++ src/components/Steps.js
@@ -1,4 +1,4 @@
-import React, { useEffect, useState } from "react";
+import React, { useEffect } from "react";
 import classes from "./Steps.module.css";
 import { useSelector } from "react-redux";
 
